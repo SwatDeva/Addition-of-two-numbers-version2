9877d84a8c89a059cb64cfad591c4f433bd50356
SwatDeva <shwethluvsall@gmail.com>
2014-12-25 10:59:57 +0000

    Initial Commit

---
+++ Addition of two numbers version2/Addition of two numbers version2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition_of_two_numbers_version2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("The sum of two numbers is " + calculator.addTwoNumbers(3, 5));
+
+        }
+    }
+}
